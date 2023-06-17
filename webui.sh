fc1b43e42cb214175ed1d0d810f3a5f74bf28fb1
Nguyễn Bá Việt <49739408+nguyenbaviet@users.noreply.github.com>
2023-06-17 19:42:51 +0700

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -172,16 +172,16 @@ then
     #     first_launch=1
     # fi
     # shellcheck source=/dev/null
-    if [[ -f "${venv_dir}"/bin/activate ]]
-    then
-        source "${venv_dir}"/bin/activate
-        "${venv_dir}/bin/python3" -m pip install --user --upgrade pip
-    else
-        printf "\n%s\n" "${delimiter}"
-        printf "\e[1m\e[31mERROR: Cannot activate python venv, aborting...\e[0m"
-        printf "\n%s\n" "${delimiter}"
-        exit 1
-    fi
+    # if [[ -f "${venv_dir}"/bin/activate ]]
+    # then
+    #     source "${venv_dir}"/bin/activate
+    #     # "${venv_dir}/bin/python3" -m pip install --user --upgrade pip
+    # else
+    #     printf "\n%s\n" "${delimiter}"
+    #     printf "\e[1m\e[31mERROR: Cannot activate python venv, aborting...\e[0m"
+    #     printf "\n%s\n" "${delimiter}"
+    #     exit 1
+    # fi
 else
     printf "\n%s\n" "${delimiter}"
     printf "python venv already activate: ${VIRTUAL_ENV}"
