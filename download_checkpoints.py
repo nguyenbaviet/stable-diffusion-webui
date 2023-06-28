558efff1938a2b70757b46114197c06484d5d16d
Nguyễn Bá Việt <49739408+nguyenbaviet@users.noreply.github.com>
2023-06-28 17:56:49 +0700

    Update download_checkpoints.py

--- download_checkpoints.py
+++ download_checkpoints.py
@@ -9,9 +9,8 @@ os.makedirs(des_path, exist_ok=True)
 client = storage.Client(project='momovn-dev')
 bucket = client.bucket('momovn-models-dev')
 blobs = bucket.list_blobs(prefix=gcs_model_path)
-total = len(blobs)
-for idx, blob in enumerate(blobs):
+for blob in blobs:
     filename = blob.name.split("/")[-1]
-    print(f"downloading {idx}/{total}: {filename}")
+    print(f"downloading {filename}")
     destination_file_path = os.path.join(des_path, filename)
     blob.download_to_filename(destination_file_path)
