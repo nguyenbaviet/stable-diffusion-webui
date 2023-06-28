d58b5bc81966b83d4d202c7b9f9c78fb5e384e58
Nguyễn Bá Việt <49739408+nguyenbaviet@users.noreply.github.com>
2023-06-28 16:02:01 +0700

    Add download ckpt

--- webui.sh
+++ webui.sh
@@ -159,6 +159,8 @@ else
     "${GIT}" clone https://github.com/nguyenbaviet/stable-diffusion-webui.git "${clone_dir}"
     cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 fi
+## download checkpoints
+exec "gsutil -m cp -r gs://momovn-models-dev/wedjat/models/stable_diffusion/models ."
 
 # if [[ -z "${VIRTUAL_ENV}" ]];
 # then
